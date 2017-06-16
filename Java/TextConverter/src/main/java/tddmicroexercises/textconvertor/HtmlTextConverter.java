8b68ea72b7a872eff25fc65ad4a4a178b7bd960b
JoeBew42 <joebew42@gmail.com>
2017-06-16 19:30:41 +0200

    first attempt

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -1,33 +1,43 @@
 package tddmicroexercises.textconvertor;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class HtmlTextConverter
 {
     private String fullFilenameWithPath;
+	private BufferedReader reader;
 
-    public HtmlTextConverter(String fullFilenameWithPath)
+	public HtmlTextConverter(String fullFilenameWithPath)
     {
         this.fullFilenameWithPath = fullFilenameWithPath;
     }
 
     public String convertToHtml() throws IOException{
-    
-	    BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath));
-	    
-	    String line = reader.readLine();
+
+		readerCreateFrom(fullFilenameWithPath);
+
+		String line = readerReadLine();
 	    String html = "";
 	    while (line != null)
 	    {
 	    	html += StringEscapeUtils.escapeHtml(line);
 	        html += "<br />";
-	        line = reader.readLine();
+	        line = readerReadLine();
 	    }
 	    return html;
 
     }
+
+	protected String readerReadLine() throws IOException {
+		return reader.readLine();
+	}
+
+	protected void readerCreateFrom(String filePath) throws FileNotFoundException {
+		reader = new BufferedReader(new FileReader(filePath));
+	}
 
 	public String getFilename() {
 		return this.fullFilenameWithPath;
--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -1,13 +1,80 @@
 package tddmicroexercises.textconvertor;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
 public class HtmlTextConverterTest {
+
+    public static final String MISSING_FILE = "missing_file";
+
     @Test
     public void foo() {
         HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+        assertEquals("foo", converter.getFilename());
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void throws_file_not_found_exception_when_file_does_not_exists() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter(MISSING_FILE);
+
+        converter.convertToHtml();
+    }
+
+    @Test
+    public void returns_empty_html_when_file_is_empty() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverterThatReturnsEmptyContent("empty.txt");
+
+        assertEquals("", converter.convertToHtml());
+    }
+
+    @Test
+    public void returns_an_html_when_file_is_not_empty() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverterThatReturnsContent("full.txt");
+
+        assertEquals("first<br />first<br />first<br />", converter.convertToHtml());
+    }
+
+    private class HtmlTextConverterThatReturnsContent extends HtmlTextConverter {
+        private int counter = 0;
+        public HtmlTextConverterThatReturnsContent(String fileName) {
+            super(fileName);
+        }
+
+        @Override
+        protected String readerReadLine() throws IOException {
+            counter++;
+            if(counter<4){
+                return "first";
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void readerCreateFrom(String filePath) throws FileNotFoundException {
+
+        }
+
+
+    }
+
+    private class HtmlTextConverterThatReturnsEmptyContent extends HtmlTextConverter {
+        public HtmlTextConverterThatReturnsEmptyContent(String fileName) {
+            super(fileName);
+        }
+
+        @Override
+        protected String readerReadLine() throws IOException {
+          return  null;
+        }
+
+        @Override
+        protected void readerCreateFrom(String filePath) throws FileNotFoundException {
+        }
+
     }
 }
